b45a5798e953dd4b397e49a2a61bfae1b995436f
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-11-08 17:29:07 +0630

    add db

--- lib/pages/add.dart
+++ lib/pages/add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_note/constant/color.dart';
 
 class AddPage extends StatelessWidget {
@@ -7,6 +8,11 @@ class AddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: cPrimary,
+        child: const Icon(FontAwesomeIcons.floppyDisk),
+      ),
       appBar: AppBar(
         elevation: 0.2,
         leading: IconButton(
@@ -21,19 +27,25 @@ class AddPage extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       backgroundColor: cBackGround,
-      body: const SafeArea(
+      body: SafeArea(
           child: Padding(
         padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
+        child: ListView(
+          children: const [
             TextField(
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               cursorHeight: 30,
               decoration: InputDecoration(
+                hintText: 'Title',
                 border: InputBorder.none,
               ),
             ),
-            TextField(maxLines: 10, style:  TextStyle(fontSize: 20),decoration: InputDecoration(border: InputBorder.none),)
+            TextField(
+              maxLines: 60,
+              style: TextStyle(fontSize: 20),
+              decoration: InputDecoration(
+                  hintText: "Someting...", border: InputBorder.none),
+            )
           ],
         ),
       )),
